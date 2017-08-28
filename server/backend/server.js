761b63d2bd8faa11bac0c3ea0fe88e84c1683c6c
Hua Wang <whloveyqt@gmail.com>
2017-08-28 12:00:48 -0700

    cleaned code and added more comments

--- server/backend/server.js
+++ server/backend/server.js
@@ -10,13 +10,8 @@ router.post('/', function(req, res, next) {
   // by contract, incoming data with key "fragments"
   var input = req.body.fragments;
 
-  // connect encoded input into result string
-  var encodedOutput = decoder.decode(input);
-
-  // var encodedOutput = '%2F%2FSample+program%0Apublic+class+HelloWorld+%7B%0A++++public+static+void+main%28String%5B%5D+args%29+%7B%0A++++++++%2F%2F+Prints+%22Hello%2C+World%22+to+the+terminal+window.%0A++++++++++System.out.println%28%22Hello%2C+World%22%29%3B%0A++++%7D%0A%7D%0A';
-
   // url decode result string
-  var output = decodeURIComponent(encodedOutput.replace(/\+/g, ' '));
+  var output = decoder.decode(input);
 
   // send decoded string back as response body
   res.status(200).send(JSON.stringify(output))
@@ -29,30 +24,29 @@ module.exports = router;
 setTimeout(function() {
 
   var post_data = JSON.stringify(
-      {fragments: ['%2F%2F+Sample+progr',
-        'program%0Apublic+',
-        'ublic+class+Hel',
-        'lass+HelloWorld',
-        'elloWorld+%7B%0A+++',
-        'd+%7B%0A++++public+',
-        'public+static+v',
-        'c+static+void+m',
-        'id+main%28String%5B',
-        '%28String%5B%5D+args%29',
-        'args%29+%7B%0A+++++++',
-        '++++++%2F%2F+Prints',
-        '%2F%2F+Prints+%22Hell',
-        'ts+%22Hello%2C+Worl',
-        '%2C+World%22+to+the',
-        '%22+to+the+termin',
-        'rminal+window.%0A',
-        'ow.%0A++++++++Sys',
-        '+++++++System.o',
-        'stem.out.printl',
-        'intln%28%22Hello%2C+W',
-        'o%2C+World%22%29%3B%0A+++',
-        'rld%22%29%3B%0A++++%7D%0A%7D%0A',
-        '%3B%0A++++%7D%0A%7D%0A']}
+      {fragments: [
+        "%2F%2FSample+progra",
+        "e+program%0Apubli",
+        "m%0Apublic+class+",
+        "ass+HelloWorld+",
+        "rld+%7B%0A++++publi",
+        "ublic+static+vo",
+        "+void+main%28Stri",
+        "String%5B%5D+args%29+",
+        "gs%29+%7B%0A++++++++%2F",
+        "++%2F%2F+Prints+%22He",
+        "Prints+%22Hello%2C+",
+        "s+%22Hello%2C+World",
+        "o%2C+World%22+to+th",
+        "d%22+to+the+termi",
+        "e+terminal+wind",
+        "l+window.%0A+++++",
+        "++++++++System.",
+        "+System.out.pri",
+        "println%28%22Hello%2C",
+        "o%2C+World%22%29%3B%0A+++",
+        "%3B%0A++++%7D%0A%7D%0A"
+      ]}
     );
 
 // An object of options to indicate where to post to
--- server/domain/decodeUrlEncodedFragments.js
+++ server/domain/decodeUrlEncodedFragments.js
@@ -1,93 +1,56 @@
-// ["l+window.%0A+++++",
-//   "gs%29+%7B%0A++++++++%2F",
-//   "%3B%0A++++%7D%0A%7D%0A",
-//   "ublic+static+vo",
-//   "++++++++System.",
-//   "d%22+to+the+termi",
-//   "s+%22Hello%2C+World",
-//   "++%2F%2F+Prints+%22He",
-//   "Prints+%22Hello%2C+",
-//   "e+terminal+wind",
-//   "rld+%7B%0A++++publi",
-//   "m%0Apublic+class+",
-//   "+void+main%28Stri",
-//   "String%5B%5D+args%29+",
-//   "println%28%22Hello%2C",
-//   "e+program%0Apubli",
-//   "++System.out.pr",
-//   "o%2C+World%22%29%3B%0A+++",
-//   "o%2C+World%22+to+th",
-//   "ass+HelloWorld+",
-//   "%2F%2FSample+progra"]
 
 module.exports = {
   decode: function(strArr) {
 
-    if (!Array.isArray(strArr) || !strArr.length) {
-      return '';
-    }
-    if (strArr.length === 1) {
-      return decodeURIComponent(strArr[0]);
-    }
-    var len = strArr.length;
-    for (var i = 0; i < len; i++) {
-      var candidateStr = [];
-      var subStr = strArr[i].substr(len-3, 3);
-      for (var j = 0; j < len; j++) {
-        if (i != j) {
-          if (strArr[j].indexOf(substr) != -1) {
-            candidateStr.push(strarr[j]);
-          }
-        }
-      }
-      for (var k = 0; k < candidateStr.length; k++) {
-        var indexK = candidateStr[k].indexOf(subStr);
-        var indexI = len - 3;
-        while(indexK >= 0) {
-          if (candidateStr[k][indexK] == candidateStr[i][indexI]) {
-            indexK--;
-            indexI--;
-          } else {
-            break;
-          }
-        }
-        if (indexK < 0) {
-          var nextStr = candidateStr[k];
-        }
+    var singleURLString = '';
+    var lastLine = '';
+    for (var i = 0; i < strArr; i++) {
+      if (decodeURIComponent(strArr[i]).length < 15) {
+        lastLine = strArr[i];
+        strArr.splice(i, 1);
       }
     }
+    for (var j = 0; j < strArr.length; i++) {
+      var remainingStrArrs = strArr.filter(function(value) {
+        return value != strArr[i];
+      });
+      if (getValidSingleString(strArr[i], remainingStrArrs) != '') {
+        singleURLString = getValidSingleString(strArr[i], remainingStrArrs);
+        break;
+      }
+    }
+    if (singleURLString == '') {
+      return lastLine;
+    }
+    if (lastLine != '') {
+      var resultArr = getCommonCount(singleURLString, lastLine);
+      if (resultArr[0]) {
+        return concatStrs(singleURLString, lastLine, resultArr[1]).replace(/\+/g, ' ');
+      } else {
+        return '';
+      }
+    }
+    return decodeURIComponent(singleURLString.replace(/\+/g, ' '));
   }
-}
+};
 
-function checkValidBeginStr3(startStr, remainingStrArrs) {
+// Helper function
+function getValidSingleString(startStr, remainingStrArrs) {
   if (!remainingStrArrs.length) {
     return startStr;
   }
-  var len = startStr.length;
-  var lastThreeChars = startStr.substr(len-3, 3);
-  var indexStartStr = len - 3;
+
   var candidateStrs = [];
   for (var i = 0; i < remainingStrArrs.length; i++) {
-    var indexI = remainingStrArrs[i].indexOf(lastThreeChars);
-    if (indexI != -1) {
-      while(indexI >= 0) {
-        if (startStr[indexStartStr] == remainingStrArrs[i][indexI]) {
-          indexI--;
-          indexStartStr--;
-        } else {
-          break;
-        }
-      }
-      if (indexI < 0) {
-        var newStartStr = startStr + remainingStrArrs[i].substring(len-indexStartStr-1);
-        var newRemainingStrArrs = remainingStrArrs.filter(function(value, index) {
-          return value != remainingStrArrs[i];
-        });
-        candidateStrs.push(checkValidBeginStr3(newStartStr, newRemainingStrArrs));
-        //console.log(newStartStr);
-      }
+
+    var resultArr = getCommonCount(startStr, remainingStrArrs[i]);
+    if (resultArr[0]) {
+      var newStartStr = concatStrs(startStr, remainingStrArrs[i], resultArr[1]);
+      var newRemainingStrArrs = remainingStrArrs.filter(function(value) {
+        return value != remainingStrArrs[i];
+      });
+      candidateStrs.push(getValidSingleString(newStartStr, newRemainingStrArrs));
     }
-    indexStartStr = len - 3;
   }
   if (!candidateStrs.length) {
     return '';
@@ -100,52 +63,66 @@ function checkValidBeginStr3(startStr, remainingStrArrs) {
   return '';
 }
 
-function getSingleURLString(strArr) {
-  var singleURLString = '';
-  var lastLine = '';
-  for (var i = 0; i < strArr; i++) {
-    if (decodeURIComponent(strArr[i]).length < 15) {
-      lastLine = strArr[i];
-      strArr.splice(i, 1);
-    }
-  }
-  for (var j = 0; j < strArr.length; i++) {
-    var remainingStrArrs = strArr.filter(function(value, index) {
-      return value != strArr[i];
-    })
-    if (checkValidBeginStr3(strArr[i], remainingStrArrs) != '') {
-      singleURLString = checkValidBeginStr3(strArr[i], remainingStrArrs);
-      break;
-    }
-  }
-  if (lastLine != '') {
-    var len = strArr.length
+// Helper function
+// If str1 and str2 have more than 3 (including 3) overlapping characters, and str2 is a legitimate subsequent string of str1,
+// return an array of two elements, with the first element being true, the second element being the number of common characters.
+function getCommonCount(str1, str2) {
 
+  var len = str1.length;
+  var lastThreeChars = str1.substr(len-3, 3);
+  var indexStr1 = len - 3;
+  var indexStr2 = str2.indexOf(lastThreeChars);
+  var resultArr = [];
+
+  if(indexStr2 != -1) {
+    while(indexStr2 >= 0) {
+      if (str1[indexStr1] == str2[indexStr2]) {
+        indexStr1--;
+        indexStr2--;
+      } else {
+        break;
+      }
+    }
+    if (indexStr2 < 0) {
+      resultArr.push(true, len - indexStr1 - 1);
+    } else {
+      resultArr.push(false, 0);
+    }
+  } else {
+    resultArr.push(false, 0);
   }
-};
+  return resultArr;
+}
+
+// Helper function
+// return a concatenated string with the overlapping characters removed.
+
+function concatStrs(str1, str2, commonCount) {
+  return str1 + str2.substring(commonCount);
+}
 
 var input1 = [
-  '%2F%2FSample+progra',
-  'e+program%0Apubli',
-  'm%0Apublic+class+',
-  'ass+HelloWorld+',
-  'rld+%7B%0A++++publi',
-  'ublic+static+vo',
-  '+void+main%28Stri',
-  'String%5B%5D+args%29+',
-  'gs%29+%7B%0A++++++++%2F',
-  '++%2F%2F+Prints+%22He',
-  'Prints+%22Hello%2C+',
-  's+%22Hello%2C+World',
-  'o%2C+World%22+to+th',
-  'd%22+to+the+termi',
-  'e+terminal+wind',
-  'l+window.%0A+++++',
-  '++++++++System.',
-  '++System.out.pr',
-  'println%28%22Hello%2C',
-  'o%2C+World%22%29%3B%0A+++',
-  '%3B%0A++++%7D%0A%7D%0A',
+  "%2F%2FSample+progra",
+  "e+program%0Apubli",
+  "m%0Apublic+class+",
+  "ass+HelloWorld+",
+  "rld+%7B%0A++++publi",
+  "ublic+static+vo",
+  "+void+main%28Stri",
+  "String%5B%5D+args%29+",
+  "gs%29+%7B%0A++++++++%2F",
+  "++%2F%2F+Prints+%22He",
+  "Prints+%22Hello%2C+",
+  "s+%22Hello%2C+World",
+  "o%2C+World%22+to+th",
+  "d%22+to+the+termi",
+  "e+terminal+wind",
+  "l+window.%0A+++++",
+  "++++++++System.",
+  "+System.out.pri",
+  "println%28%22Hello%2C",
+  "o%2C+World%22%29%3B%0A+++",
+  "%3B%0A++++%7D%0A%7D%0A"
 ];
 
 var input2 = [
@@ -195,10 +172,4 @@ var testInput = [
   'ass+HelloWorld+',
 ];
 
-var startStr = '%2F%2FSample+progra';
-var testArr = ['%2F%2FSample+progra'];
-testArr.splice(0, 1);
-console.log(testArr);
-var newArr = testArr.filter(function(value, index) {
-  return value != startStr;
-});
+console.log(module.exports.decode(input1));
